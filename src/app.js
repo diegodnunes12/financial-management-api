718f2ec7976f7d9f223e39b3c0feca652960abc9
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-07-03 19:58:16 -0300

    Alterando o banco

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 require('./db/mongoose')
 const orderRouter = require('./routes/order')
-const categoryRouter = require('./routes/category')
+//const categoryRouter = require('./routes/category')
 
 const app = express()
 
 app.use(express.json())
 app.use(orderRouter)
-app.use(categoryRouter)
+//app.use(categoryRouter)
 
 app.get('', (req, res) => {
     
