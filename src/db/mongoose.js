01826625ca50106b183f181f0fc905a66d4497a7
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-07-04 11:16:17 -0300

    ALterando configuração no mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://diegonunes:gfW0MMOWXLT25sPk@cluster0.c2f85.gcp.mongodb.net/financial?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useCreateIndex:true,
-    useFindAndModify:false
+    useFindAndModify:false,
+    useUnifiedTopology: true
 })
 
 //mongodb+srv://diegonunes:<password>@cluster0.c2f85.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority
